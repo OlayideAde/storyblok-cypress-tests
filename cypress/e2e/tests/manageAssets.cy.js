ad7630faa253f04f8c199777987a8fc4db37bf79
Olayide <laide.ademeso@gmail.com>
2023-12-13 20:00:17 +0100

    update tests

--- cypress/e2e/tests/manageAssets.cy.js
+++ cypress/e2e/tests/manageAssets.cy.js
@@ -10,87 +10,99 @@ let assetName;
 const email = Cypress.env("email");
 const password = Cypress.env("password");
 
-
 describe("Manage Assets tab e2e tests", () => {
-    before(() => {
-        loginPage = new LoginPage();
-        dashboardPage = new DashboardPage();
-        assetsPage = new AssetsPage();
+  before(() => {
+    loginPage = new LoginPage();
+    dashboardPage = new DashboardPage();
+    assetsPage = new AssetsPage();
+  });
 
-        //login to dashboard
-        cy.visit("/");
-        loginPage.fillLoginForm(email, password);
-        loginPage.getLoginButton().click();
-        dashboardPage.getSpace().click();
-    })
+  describe("Positve tests", () => {
+    beforeEach(() => {
+      //login to dashboard
+      cy.visit("/");
+      loginPage.fillLoginForm(email, password);
+      loginPage.getLoginButton().click();
 
-    describe("Positve tests", () => {
-        beforeEach(() => {
-            dashboardPage.getAssetsTab().click();
-        })
+      //open assets tab
+      dashboardPage.getSpace().click();
+      dashboardPage.getAssetsTab().click();
+    });
 
-        it("should verify that user can register a public asset", () => {
-            //upload file
-            assetsPage.getUploadButton().click();
-            cy.get('input#file').selectFile('cypress/fixtures/downloadcat.jpeg', { force:true });
-            
-            //name asset
-            assetName = faker.random.word();
-            assetsPage.getAssetNameField().clear().type(assetName);
-            
-            //check that privacy is toggled to public
-           // assetsPage.getPrivacyButton().within(() => {
-            //    cy.get('input#asset-is-private-0').its('value').should('eq', "false")
-            //})
+    it("should verify that user can register a public asset", () => {
+      //upload file
+      assetsPage.getUploadButton().click();
+      cy.get("input#file").selectFile("cypress/fixtures/downloadcat.jpeg", {
+        force: true,
+      });
 
-            //click upload
-            assetsPage.getSubmitButton().click()
-            
-            //verify asset is visible on dashboard
-            assetsPage.getAssetItem(0).should("be.visible").within(() => {
-                //verify name
-                cy.get('span[data-testid="asset-name"]').should('contain', assetName)
-                //verify asset has an image
-                cy.get('div.asset-list-item-preview__image').within(() => {
-                    cy.get('img').should('have.attr', "src")
-                })
-            })
-        })
+      //name asset
+      assetName = faker.random.word();
+      assetsPage.getAssetNameField().clear().type(assetName);
 
-        it("should verify that user can register a private asset", () => {
-            //upload file
-            assetsPage.getUploadButton().click();
-            cy.get('input#file').selectFile('cypress/fixtures/downloaddog.jpeg', { force:true })
-            //name asset
-            assetName = faker.random.word();
-            assetsPage.getAssetNameField().clear().type(assetName);
-            //check that privacy is toggled to private
-            assetsPage.getPrivacyButton().click();
-            
-            //click upload
-            assetsPage.getSubmitButton().click()
-            //verify asset is visible on dashboard
-            assetsPage.getAssetItem(0).should("be.visible").within(() => {
-                //verify name
-                cy.get('span[data-testid="asset-name"]').should('contain', assetName)
-                //verify privacy 
-                cy.get('div.asset-private-preview').within(() => {
-                    cy.get('p').should('have.text', "Private Asset")
-                })
-            })
-        })
+      //click upload
+      assetsPage.getSubmitButton().click();
 
-        it("should verify that user can delete an asset", () => {
-            //select asset
-            assetsPage.getAssetItem(1).click();
-            //click delete icon
-            assetsPage.getDeleteAssetButton().click();
-            //verify delete modal is visibe
-            assetsPage.getDeleteModal().should('be.visible')
-            //click confirm delete button
-            assetsPage.getConfirmDeleteButton().click()
-            //verify that notifcation is displayed
-            assetsPage.getNotification().should('be.visble').and('contain', "Success")
-        })
-    })
-})
+      //verify asset is visible on dashboard
+      cy.wait(9000);
+
+      assetsPage
+        .getAssetItem(0)
+        .should("be.visible")
+        .within(() => {
+          //verify name
+          cy.contains(assetName.toLowerCase());
+
+          //verify asset has an image
+          cy.get("div.asset-list-item-preview__image")
+            .contains("img")
+            .should("have.attr", "src");
+        });
+    });
+
+    it("should verify that user can register a private asset", () => {
+      //upload file
+      assetsPage.getUploadButton().click();
+      cy.get("input#file").selectFile("cypress/fixtures/downloaddog.jpeg", {
+        force: true,
+      });
+      //name asset
+      assetName = faker.random.word();
+      assetsPage.getAssetNameField().clear().type(assetName);
+
+      //toggle privacy settings to private
+      assetsPage.getPrivacyButton().click();
+
+      //click upload
+      assetsPage.getSubmitButton().click();
+
+      //verify asset is visible on dashboard
+      cy.wait(9000);
+      assetsPage
+        .getAssetItem(0)
+        .should("be.visible")
+        .within(() => {
+          //verify name of asset
+          cy.contains(assetName.toLowerCase());
+          //verify text is shown for private asset
+          cy.contains("Private Asset");
+        });
+    });
+
+    it("should verify that user can delete an asset", () => {
+      //select asset
+      assetsPage.getAssetItem(1).click();
+      //click delete icon
+      assetsPage.getDeleteAssetButton().click();
+      //verify delete modal is visibe
+      assetsPage.getDeleteModal().should("be.visible");
+      //click confirm delete button
+      assetsPage.getConfirmDeleteButton().click();
+      //verify that notifcation is displayed
+      assetsPage
+        .getNotification()
+        .should("be.visble")
+        .and("contain", "Success");
+    });
+  });
+});
