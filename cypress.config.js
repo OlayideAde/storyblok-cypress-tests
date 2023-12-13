ae0a1325894842214cae3a51439f3dbb5d301c9f
Olayide <laide.ademeso@gmail.com>
2023-12-13 14:48:12 +0100

    Cypress setup

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+    baseUrl: "https://app.storyblok.com",
+    specPattern: ["cypress/e2e/tests/*.js"],
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "./test-reports/",
+      charts: true,
+      code: false,
+    },
+  },
+  retries: 2,
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 8000
+});
