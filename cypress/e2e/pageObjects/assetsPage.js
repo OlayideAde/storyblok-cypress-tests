a659c27c342cd0c90c81474013b4d5b681de73eb
Olayide <laide.ademeso@gmail.com>
2023-12-13 15:36:47 +0100

    Update page object

--- cypress/e2e/pageObjects/assetsPage.js
+++ cypress/e2e/pageObjects/assetsPage.js
@@ -8,18 +8,30 @@ export class AssetsPage {
     }
 
     getPrivacyButton() {
-        return cy.get('input#asset-is-private-0');
+        return cy.get('.sb-toggle__label');
     }
 
     getSubmitButton(){
-        return cy.get('button.sb-button').contains("Upload");
+        return cy.get('button[type="submit"]').should('have.text', "Upload");
     }
 
     getAssetItem(index){
         return cy.get('div.assets-list-item').eq(index)
     }
 
-    getReplaceAssetButton(){
-        return cy.get('button[aria-label="Replace asset"]');
+    getDeleteAssetButton(){
+        return cy.get('button[aria-label="Delete asset"]');
+    }
+
+    getDeleteModal(){
+        return cy.get('.modal-delete-caution-confirm')
+    }
+
+    getConfirmDeleteButton() {
+        return cy.get('.sb-button--danger')
+    }
+
+    getNotification(){
+        return cy.get('.custom_notification')
     }
 }
